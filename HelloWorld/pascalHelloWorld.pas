34699d93ba83df7b59f6953b8a44e7f14de7ca26
Loona <r.arswendo.r@gmail.com>
2021-09-21 16:27:27 +0700

    Improve HelloWorld Project

--- HelloWorld/pascalHelloWorld.pas
+++ HelloWorld/pascalHelloWorld.pas
@@ -1,4 +1,25 @@
 program pascalHelloWorld;
+
+var
+    age: integer = 20;
+    myName: String;
+    listOfNumbers: array [0..9, 0..8] of integer;
+    i, j, x, y: integer;
+
 begin
-    writeln('Hello World!');
+    myName := 'Luna';
+    writeln('Hello, ', myName, ' Your age is: ', age);
+
+    for i := 0 to 9 do
+    begin
+        for x := 0 to 8 do
+            listOfNumbers[i, x] := i + x; 
+    end;
+
+    for j := 0 to 9 do
+    begin
+        for y := 0 to 8 do
+            write(listOfNumbers[j, y]:2, '  ');
+        writeln;
+    end;
 end.
