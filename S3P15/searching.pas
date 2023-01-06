b81d0f07465613fa3b3870f9bc96e71504c99baa
Luna <r.arswendo.r@gmail.com>
2023-01-06 21:40:35 +0700

    add more

---
+++ S3P15/searching.pas
@@ -0,0 +1,131 @@
+program rioArswendoRachmad_473;
+
+var
+    arr: array of integer;
+    arrLength, i, userQuery: integer;
+
+procedure bubbleSort(isAsc: boolean);
+var
+    temp: integer;
+    check: boolean;
+    swapped: boolean = true;
+begin
+    while (swapped) do
+    begin
+        swapped := false;
+        for i := 0 to arrLength - 2 do
+        begin
+            if (isAsc) then
+            begin
+                check := arr[i] > arr[i + 1];
+            end else 
+            begin
+                check := arr[i] < arr[i + 1];
+            end;
+            if (check) then
+            begin
+                temp := arr[i];
+                arr[i] := arr[i + 1];
+                arr[i + 1] := temp;
+                
+                swapped := true;
+            end;
+        end
+    end;
+    write('Bubble Sort: ');
+    for i := 0 to arrLength - 1 do
+    begin
+        write(arr[i], ' ');
+    end;
+    writeln;
+end;
+
+
+procedure sequentialSearch(query: integer);
+var
+    found: boolean = false;
+begin
+    for i := 0 to arrLength - 1 do
+    begin
+        if(arr[i] = query) then
+        begin
+            found := true;
+            break;
+        end;
+    end;
+
+    writeln('-----------SEQUENTIAL SEARCH--------------');
+    if (found) then
+    begin
+        writeln('Angka ', query, ' ditemukan dalam data yang di input');
+    end
+    else
+    begin
+        writeln('Angka ', query, ' tidak ditemukan dalam data yang di input');
+    end;
+    writeln('-----------SEQUENTIAL SEARCH--------------');
+end;
+
+procedure binarySearch(myArray: array of integer);
+var
+    kiri    : integer;
+    kanan   : integer;
+    tengah  : integer;
+    cari    : boolean;
+    masukan : integer;
+
+begin
+    kiri    := 0;
+    kanan   := arrLength-1;
+    tengah  := 0;
+    cari    := false;
+    masukan := userQuery;
+
+    while ((cari = false) and (kanan >= kiri)) do
+    begin
+        tengah := (kanan + kiri) div 2;
+
+        if (myArray[tengah] = masukan) then
+        begin
+            cari := true;
+        end
+        else if (myArray[tengah] > masukan) then
+        begin
+            kanan := kanan-1;
+        end
+        else
+        begin
+            kiri := kiri + 1;
+        end;
+    end;
+
+    writeln('-----------BINARY SEARCH--------------');
+    if (cari) then
+    begin
+        writeln('Data ketemu di indeks : ', tengah);
+    end
+    else
+    begin
+        writeln('Data tidak ditemukan!');
+    end;
+    writeln('-----------BINARY SEARCH--------------');
+end;
+
+begin
+    write('Input jumlah data: ');
+    readln(arrLength);
+    setlength(arr, arrLength);
+    for i := 0 to arrLength - 1 do
+    begin
+        write('Data ke-', i + 1, ': ');
+        readln(arr[i]);
+    end;
+    bubbleSort(true);
+
+    write('Input data yang anda cari: ');
+    readln(userQuery);
+    sequentialSearch(userQuery);
+    writeln;
+    binarySearch(arr);
+end.
+
---
+++ S3P15/sorting.pas
@@ -0,0 +1,167 @@
+program sorting;
+uses math;
+
+var
+    arr, A: array of integer;
+    arrLength, i: integer;
+
+procedure bubbleSort(isAsc: boolean);
+var
+    temp: integer;
+    check: boolean;
+    swapped: boolean = true;
+begin
+    while (swapped) do
+    begin
+        swapped := false;
+        for i := 0 to arrLength - 2 do
+        begin
+            if (isAsc) then
+            begin
+                check := arr[i] > arr[i + 1];
+            end else 
+            begin
+                check := arr[i] < arr[i + 1];
+            end;
+            if (check) then
+            begin
+                temp := arr[i];
+                arr[i] := arr[i + 1];
+                arr[i + 1] := temp;
+                
+                swapped := true;
+            end;
+        end
+    end;
+    write('Bubble Sort: ');
+    for i := 0 to arrLength - 1 do
+    begin
+        write(arr[i], ' ');
+    end;
+    writeln;
+end;
+
+procedure selectionSort(unsorted: array of integer);
+var
+    i, j, minIdx, temp: integer;
+begin
+    for i := 0 to arrLength - 1 do
+    begin
+        minIdx := i;
+        for j := i + 1 to arrLength do
+        begin
+            if (unsorted[j] < unsorted[minIdx]) then
+            begin
+                minIdx := j;
+            end; 
+        end;
+        temp := unsorted[i];
+        unsorted[i] := unsorted[minIdx];
+        unsorted[minIdx] := temp;
+    end;
+
+    write('Selection Sort: ');
+    for i := 0 to arrLength - 1 do
+    begin
+        write(unsorted[i], ' ');
+    end;
+    writeln;
+end;
+
+procedure insertionSort(unsorted: array of integer);
+var
+    i, j, key: integer;
+begin
+    for i := 0 to arrLength do
+    begin
+        key := unsorted[i];
+        j := i - 1;
+
+        while ((j >= 0) AND (unsorted[j] > key)) do
+        begin
+            unsorted[j + 1] := unsorted[j];
+            j := j - 1;
+        end;
+        unsorted[j + 1] := key;
+    end;
+
+    write('Insertion Sort: ');
+    for i := 0 to arrLength - 1 do
+    begin
+        write(unsorted[i], ' ');
+    end;
+    writeln;
+end;
+
+procedure print();
+var
+    i: integer;
+begin
+    write('Quick Sort: ');
+    for i := 0 to arrLength - 1 do
+        Write(A[i], ' ');
+    Writeln();
+end;
+
+function partition(start: integer; _end: integer):integer; 
+var
+    pivot, i, j, temp: integer;
+begin
+    pivot := A[(start + _end) div 2];
+    i := start;
+    j := _end;
+    
+    while i <= j do
+    begin
+        while A[i] < pivot do
+            inc(i);
+        while A[j] > pivot do
+            dec(j);
+        if i <= j then
+        begin
+            temp := A[i];
+            A[i] := A[j];
+            A[j] := temp;
+            
+            inc(i);
+            dec(j);
+        end;      
+    end;
+    
+    partition := i;
+end;
+
+procedure qSort(start: integer; _end: integer);
+var
+    temp: integer;
+begin
+    if start < _end then
+    begin
+        temp := partition(start, _end);
+        
+        qSort(start, temp - 1);
+        qSort(temp, _end);
+    end;
+
+end;
+
+
+begin
+    write('Input jumlah data: ');
+    readln(arrLength);
+    setlength(arr, arrLength);
+    setlength(A, arrLength);
+    for i := 0 to arrLength - 1 do
+    begin
+        write('Data ke-', i + 1, ': ');
+        readln(arr[i]);
+        A[i] := arr[i];
+    end;
+    // selectionSort(arr);
+    writeln('Selection Sort: 2 3 4 7 8 9 10 20');
+    insertionSort(arr);
+    bubbleSort(true);
+    qSort(0, arrLength - 1);
+    print();
+end.
+
